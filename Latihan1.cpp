18f0ce3871592f07a2fc51e20f6426ac466f72db
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-08 12:32:13 +0700

    first commit

---
+++ Latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+//Numeric Luas, P, L 
+double luas, P, L;
+//Display "Masukan Panjangnya = "
+cout << "Masukan Panjangnya = " ;
+//Accept P
+cin >> P;
+//Display "Masukan Lebarnya = "
+cout << "Masukan Lebarnya = " ;
+//Accept L
+cin >> L;
+//Compute Luas = P*L 
+luas = P*L;
+//Display "Luasnya = " + Luas
+cout << "Luasnya = "<< luas;
+}//end
